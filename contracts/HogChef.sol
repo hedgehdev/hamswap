e974c86631dd14c330e7a71ab9236ceda21b79b6
hedgehdev <hedgehogdev@protonmail.com>
2021-12-12 16:06:20 +0800

    add HogChef to support LP staking, reduce deposit/withdraw/deploy gas, support dynamic hogPerBlock reward change

---
+++ contracts/HogChef.sol
@@ -0,0 +1,301 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/EnumerableSet.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+// Copied from https://github.com/hogswap/hogswap/blob/master/contracts/MasterChef.sol
+// Modified by hogletdev
+
+// HogChef is where liquidity providers on HamSwap can stake their LP tokens for HOG rewards
+// The ownership of HogChef will be transferred to a governance smart contract once HOG has been sufficiently distributed
+
+
+contract HogChef is Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+    // Info of each user.
+    struct UserInfo {
+        uint256 amount; // How many LP tokens the user has provided.
+        uint256 rewardDebt; // Reward debt. See explanation below.
+        //
+        // We do some fancy math here. Basically, any point in time, the amount of HOGs
+        // entitled to a user but is pending to be distributed is:
+        //
+        //   pending reward = (user.amount * pool.accHogPerShare) - user.rewardDebt
+        //
+        // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
+        //   1. The pool's `accHogPerShare` (and `lastRewardBlock`) gets updated.
+        //   2. User receives the pending reward sent to his/her address.
+        //   3. User's `amount` gets updated.
+        //   4. User's `rewardDebt` gets updated.
+    }
+    // Info of each pool.
+    struct PoolInfo {
+        IERC20 lpToken; // Address of LP token contract.
+        uint256 allocPoint; // How many allocation points assigned to this pool. HOGs to distribute per block.
+        uint256 lastRewardBlock; // Last block number that HOGs distribution occurs.
+        uint256 accHogPerShare; // Accumulated HOGs per share, times 1e12. See below.
+    }
+    // The HOG TOKEN!
+    IERC20 public hog;
+    // Dev address.
+    address public devaddr;
+    // Block number when bonus HOG period ends.
+    uint256 public bonusEndBlock;
+    // HOG tokens created per block.
+    uint256 public hogPerBlock;
+    // Bonus muliplier for early hog makers.
+    uint256 public bonusMultiplier;
+    // Info of each pool.
+    PoolInfo[] public poolInfo;
+    // Info of each user that stakes LP tokens.
+    mapping(uint256 => mapping(address => UserInfo)) public userInfo;
+    // Total allocation poitns. Must be the sum of all allocation points in all pools.
+    uint256 public totalAllocPoint = 0;
+    // The block number when HOG mining starts.
+    uint256 public startBlock;
+    uint256 public accHogReward;
+    uint256 public lastHogPerBlock;
+    uint256 public lastHogPerBlockHeight;
+    uint256 public lastAccHogReward;
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event EmergencyWithdraw(
+        address indexed user,
+        uint256 indexed pid,
+        uint256 amount
+    );
+
+    constructor(
+        address _hog,
+        address _devaddr,
+        uint256 _hogPerBlock,
+        uint256 _startBlock,
+        uint256 _bonusMultiplier,
+        uint256 _bonusEndBlock
+    ) public {
+        hog = IERC20(_hog);
+        devaddr = _devaddr;
+        hogPerBlock = _hogPerBlock;
+        startBlock = _startBlock;
+        bonusMultiplier = _bonusMultiplier;
+        bonusEndBlock = _bonusEndBlock;
+    }
+
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+
+    // Add a new lp to the pool. Can only be called by the owner.
+    // XXX DO NOT add the same LP token more than once. Rewards will be messed up if you do.
+    function add(
+        uint256 _allocPoint,
+        IERC20 _lpToken,
+        bool _withUpdate
+    ) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 lastRewardBlock =
+            block.number > startBlock ? block.number : startBlock;
+        totalAllocPoint = totalAllocPoint.add(_allocPoint);
+        poolInfo.push(
+            PoolInfo({
+                lpToken: _lpToken,
+                allocPoint: _allocPoint,
+                lastRewardBlock: lastRewardBlock,
+                accHogPerShare: 0
+            })
+        );
+    }
+
+    // Update the given pool's HOG allocation point. Can only be called by the owner.
+    function set(
+        uint256 _pid,
+        uint256 _allocPoint,
+        bool _withUpdate
+    ) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(
+            _allocPoint
+        );
+        poolInfo[_pid].allocPoint = _allocPoint;
+    }
+
+    // Return reward multiplier over the given _from to _to block.
+    function getMultiplier(uint256 _from, uint256 _to)
+        public
+        view
+        returns (uint256)
+    {
+        if (_from <= startBlock) {
+            _from = startBlock;
+            if (_to <= startBlock) {
+                _to = startBlock;
+            }
+        }
+        if (_to <= bonusEndBlock) {
+            return _to.sub(_from).mul(bonusMultiplier);
+        } else if (_from >= bonusEndBlock) {
+            return _to.sub(_from);
+        } else {
+            return
+                bonusEndBlock.sub(_from).mul(bonusMultiplier).add(
+                    _to.sub(bonusEndBlock)
+                );
+        }
+    }
+
+    function remainingHog() external view returns (uint256) {
+        uint distributedHog = getMultiplier(lastHogPerBlockHeight, block.number).mul(hogPerBlock).add(lastAccHogReward);
+        if (accHogReward > distributedHog) {
+            return accHogReward - distributedHog;
+        }
+        return 0;
+    }
+
+    // View function to see pending HOGs on frontend.
+    function pendingHog(uint256 _pid, address _user)
+        external
+        view
+        returns (uint256)
+    {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accHogPerShare = pool.accHogPerShare;
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
+            uint256 multiplier =
+                getMultiplier(pool.lastRewardBlock, block.number);
+            uint256 hogReward =
+                multiplier.mul(hogPerBlock).mul(pool.allocPoint).div(
+                    totalAllocPoint
+                );
+            accHogPerShare = accHogPerShare.add(
+                hogReward.mul(1e12).div(lpSupply)
+            );
+        }
+        return user.amount.mul(accHogPerShare).div(1e12).sub(user.rewardDebt).mul(9).div(10);
+    }
+
+    // Update reward vairables for all pools. Be careful of gas spending!
+    function massUpdatePools() public {
+        uint256 length = poolInfo.length;
+        for (uint256 pid = 0; pid < length; ++pid) {
+            updatePool(pid);
+        }
+    }
+
+    // Update reward variables of the given pool to be up-to-date.
+    function updatePool(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        if (block.number <= pool.lastRewardBlock) {
+            return;
+        }
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (lpSupply == 0) {
+            pool.lastRewardBlock = block.number;
+            return;
+        }
+        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+        uint256 hogReward =
+            multiplier.mul(hogPerBlock).mul(pool.allocPoint).div(
+                totalAllocPoint
+            );
+        pool.accHogPerShare = pool.accHogPerShare.add(
+            hogReward.mul(1e12).div(lpSupply)
+        );
+        pool.lastRewardBlock = block.number;
+    }
+
+    // Deposit LP tokens to MasterChef for HOG allocation.
+    function deposit(uint256 _pid, uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        updatePool(_pid);
+        if (user.amount > 0) {
+            uint256 pending =
+                user.amount.mul(pool.accHogPerShare).div(1e12).sub(
+                    user.rewardDebt
+                );
+            safeHogTransfer(devaddr, pending.div(10));
+            safeHogTransfer(msg.sender, pending.sub(pending.div(10)));
+        }
+        pool.lpToken.safeTransferFrom(
+            address(msg.sender),
+            address(this),
+            _amount
+        );
+        user.amount = user.amount.add(_amount);
+        user.rewardDebt = user.amount.mul(pool.accHogPerShare).div(1e12);
+        emit Deposit(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw LP tokens from MasterChef.
+    function withdraw(uint256 _pid, uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        require(user.amount >= _amount, "withdraw: not good");
+        updatePool(_pid);
+        uint256 pending =
+            user.amount.mul(pool.accHogPerShare).div(1e12).sub(
+                user.rewardDebt
+            );
+        safeHogTransfer(devaddr, pending.div(10));
+        safeHogTransfer(msg.sender, pending.sub(pending.div(10)));
+        user.amount = user.amount.sub(_amount);
+        user.rewardDebt = user.amount.mul(pool.accHogPerShare).div(1e12);
+        pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        emit Withdraw(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw without caring about rewards. EMERGENCY ONLY.
+    function emergencyWithdraw(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        pool.lpToken.safeTransfer(address(msg.sender), user.amount);
+        emit EmergencyWithdraw(msg.sender, _pid, user.amount);
+        user.amount = 0;
+        user.rewardDebt = 0;
+    }
+
+    // Safe hog transfer function, just in case if rounding error causes pool to not have enough HOGs.
+    function safeHogTransfer(address _to, uint256 _amount) internal {
+        uint256 hogBal = hog.balanceOf(address(this));
+        if (_amount > hogBal) {
+            hog.safeTransfer(_to, hogBal);
+        } else {
+            hog.safeTransfer(_to, _amount);
+        }
+    }
+
+    // Update dev address by the previous dev.
+    function dev(address _devaddr) public {
+        require(msg.sender == devaddr, "dev: wut?");
+        devaddr = _devaddr;
+    }
+
+    function setStartBlock(uint256 _startBlock) public onlyOwner {
+        require(block.number < startBlock, "start block can not be modified after it has passed");
+        require(block.number < _startBlock, "new start block needs to be in the future");
+        startBlock = _startBlock;
+    }
+    
+    function setHogPerBlock(uint256 _hogPerBlock) public onlyOwner {
+        massUpdatePools();
+        lastAccHogReward = getMultiplier(lastHogPerBlockHeight, block.number).mul(hogPerBlock).add(lastAccHogReward);
+        hogPerBlock = _hogPerBlock;
+        lastHogPerBlockHeight = block.number;
+    }
+
+    function depositHogReward(uint amount) public {
+        hog.safeTransferFrom(msg.sender, address(this), amount);
+        accHogReward = accHogReward.add(amount);
+    }
+}
